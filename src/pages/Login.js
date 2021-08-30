8754d0a1b23cdc357ecbeff62e9f976cf2047ca4
Raione Nascimento <56979345+RaioneNascimento@users.noreply.github.com>
2021-08-30 09:03:22 -0300

    criacao componente login 1:53:44

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import logo from '../assets/logo/logo.svg'
+import logo from '../assets/logo/logo.svg';
+import mail from '../assets/logo/mail.svg';
+import lock from '../assets/logo/lock.svg';
 
 export const Login = () => {
   return (
-    <div>
-      <img src={logo} alt="logo" />
-      <form action="">
-        <input type="text" name="login" placeholder="Informe seu email" />
-        <input type="password" name="senha" placeholder="Informe sua senha" />
+    <div className="container-login">
+      <img src={logo} alt="logo" className="logo" />
+      <form>
+        <div className="input">
+          <img src={mail} alt="ícone e-mail" />
+          <input type="text" name="login" placeholder="Informe seu email" />
+        </div>
+        <div className="input">
+          <img src={lock} alt="ícone cadeado" />
+          <input type="password" name="senha" placeholder="Informe sua senha" />
+        </div>
+
         <button>Login</button>
       </form>
     </div>
