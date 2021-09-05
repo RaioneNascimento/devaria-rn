a3b3673e9274e843bd05bf8adfe4cca01c5c67d2
Raione Nascimento <56979345+RaioneNascimento@users.noreply.github.com>
2021-09-05 14:13:31 -0300

    termino tela devaria

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo/logo.svg';
 import mail from '../assets/logo/mail.svg';
 import lock from '../assets/logo/lock.svg';
+import { Input } from '../components/input';
 
 export const Login = () => {
+  const [login, setLogin] = useState('');
+  const [senha, setSenha] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
+  const consultaLogin = evento => {
+    evento.preventDefault();
+    setLoading(true);
+    console.log(login);
+    console.log(senha);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000)
+  }
+
   return (
     <div className="container-login">
       <img src={logo} alt="logo" className="logo" />
       <form>
-        <div className="input">
-          <img src={mail} alt="ícone e-mail" />
-          <input type="text" name="login" placeholder="Informe seu email" />
-        </div>
-        <div className="input">
-          <img src={lock} alt="ícone cadeado" />
-          <input type="password" name="senha" placeholder="Informe sua senha" />
-        </div>
+        <Input
+          srcImg={mail}
+          altImg={"Icone email"}
+          inputType="text"
+          inputName="login"
+          inputPlaceholder="Informe seu email"
+          value={login}
+          setValue={setLogin}
+        />
 
-        <button>Login</button>
+        <Input
+          srcImg={lock}
+          altImg={"Icone cadeado"}
+          inputType="password"
+          inputName="senha"
+          inputPlaceholder="Informe sua senha"
+          value={senha}
+          setValue={setSenha}
+        />
+
+        <button onClick={consultaLogin} disabled={isLoading}>{isLoading === true ? '... Carregando' : 'Entrar'}</button>
       </form>
     </div>
   );
